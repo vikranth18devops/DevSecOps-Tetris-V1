ed2271712bb2936e7dde4eac264e7e4e90289809
Vikranth Sunkarpally <50102593+vikranth18devops@users.noreply.github.com>
2024-02-19 22:22:49 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aarvik250222" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Eks-terraform/terraform.tfstate"
     region = "ap-southeast-2"
   }
 }
