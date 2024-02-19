f96da7a7f9cd24503521f0f24d008bbe0114efb6
apple <apple@apples-MacBook-Air.local>
2024-02-19 13:12:44 +0530

    first.1 commit

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -67,7 +67,7 @@ resource "aws_instance" "web" {
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
 
   tags = {
-    Name = "Jenkins-argo"
+    Name = "Jenkins"
   }
 
   root_block_device {
